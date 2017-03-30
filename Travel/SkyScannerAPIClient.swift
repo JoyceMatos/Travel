deaab14c2b00cc7e3cf6ec0627a0d057ffcff6a3
JoyceMatos <j.matosjoyce@gmail.com>
2017-03-29 23:32:10 -0400

    Get quotes for flights

--- Travel/SkyScannerAPIClient.swift
+++ Travel/SkyScannerAPIClient.swift
@@ -18,10 +18,10 @@ class SkyScannerAPIClient {
         
         Alamofire.request("http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/US/usd/en-US/us/anywhere/anytime/anytime?apikey=" + skyScannerAPIKey).responseJSON { (response) in
             
-            print(response.request)  // original URL request
-            print(response.response) // HTTP URL response
-            print(response.data)     // server data
-            print(response.result)   // result of response serialization
+//            print(response.request)  // original URL request
+//            print(response.response) // HTTP URL response
+//            print(response.data)     // server data
+//            print(response.result)   // result of response serialization
             
             if let JSON = response.result.value {
                 print("JSON: \(JSON)")
@@ -29,7 +29,22 @@ class SkyScannerAPIClient {
             
         }
         
-        
+    }
+    
+    class func getQuotes(budget amount: Int, completion: () -> Void) {
+        Alamofire.request("http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/US/usd/en-US/us/anywhere/anytime/anytime?apikey=" + skyScannerAPIKey).responseJSON { (response) in
+            
+            //            print(response.request)  // original URL request
+            //            print(response.response) // HTTP URL response
+            //            print(response.data)     // server data
+            //            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+            
+        }
+
         
     }
     
