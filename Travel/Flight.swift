a0cd2c37e18fae38cfc2c0de618ed084daee270b
JoyceMatos <j.matosjoyce@gmail.com>
2017-05-01 22:03:03 -0400

    Add flight model

--- Travel/Flight.swift
+++ Travel/Flight.swift
@@ -2,15 +2,21 @@
 //  Flight.swift
 //  Travel
 //
-//  Created by Joyce Matos on 4/20/17.
+//  Created by Joyce Matos on 5/1/17.
 //  Copyright © 2017 Joyce Matos. All rights reserved.
 //
 
 import Foundation
 
+// NOTE: - Information needed for display
+
 struct Flight {
     
-    let carrierID: Int
-    let name: String
+    let destination: Place
+    let quote: Int
+    let direct: Bool
+    let airlines: [Carrier]
+    let outboundDepartureDate: String
+    let inboundDepartureDate: String
     
 }
